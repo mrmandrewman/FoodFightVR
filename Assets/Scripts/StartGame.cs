7078563efab3ce267209979a28dbe3cd0cfee482
Andrew Freebrey <mrmandrewman@users.noreply.github.com>
2019-12-04 19:02:14 +0000

    Added TimerScript
    Game Timer

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+	public float timeLeft = 180.0f;
+	public Text startText;
+
+
+	void Update()
+	{
+		timeLeft -= 1 * Time.deltaTime;
+		startText.text = (timeLeft).ToString("0");
+		if (timeLeft < 0)
+		{
+			Application.Quit();
+		}
+	}
+}
